ab4c3cf41eb90ba1dfd496a5ecb0aefaf323ca92
Patricio Glenn Peña <Patricio Glenn Pea>
2017-07-04 02:40:10 -0300

    Borre los Modulos, la verdad es que me parece que hay que buscar otra forma de colaborar u organizarse mejor...
    Reutilize el codigo que habias hecho y arme una funcion para que haga todo de una, hay varias cosas por mejorar, estaria bueno que la pruebes y veas que se puede mejorar, cambiar, etc...
    
    Te advierto que al menos en mi maquina, se demora media hora para una imagen del tamaño de la del profe, le puse que haga console.log del array que se va procesando como para ver algo mientras lo hace XD
    
    incorpore al proyecto la pagina que nos paso el profe, esa que generaba su aplicacion java, como para poder probar como queda la imagen y para que vayamos viendo y modificando ese codigo ya que la idea seria "reutilizarlo"
    
    Saludos
    Pato

--- app.js
+++ app.js
@@ -6,21 +6,87 @@ var Jimp = require("jimp");
 
 var puerto = 3030;
 
-
-var angel = require("./modules/angel");
-var patricio = require("./modules/patricio");
-
-
-	
 app.get("/",function(req, res){
 
-	angel.test();
+    let width=0;
+    let height=0;
+    let size=256;
+    let array = [];
+    let i = 0;
 
-	res.json({ok_chaval:true, hello: angel.hello() });
+    Jimp.read("img/image.jpg", function (err, image) {
+        if (err) throw err;
+        width = this.bitmap.width; 
+        height = this.bitmap.height; 
+		console.log(width);
+		console.log(height);
+            while (size < width || size < height){
+            size = size * 2;
+			console.log(size);
+        }
+        this.contain(size, size, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
+      array.push("t");
+      this.write("img/t.jpg");
+      element=array[i];
+      procesarMosaicos (element);
+    });
 
+    function procesarMosaicos (element) {
+
+        console.log (array.toString());
+        
+        Jimp.read('img/'+element+'.jpg', function(err, foto){
+            if(err) throw err;
+                
+            size = this.bitmap.width; 
+            if (size>256) {
+
+                var copia1 = foto.clone();
+                var copia2 = foto.clone();
+                var copia3 = foto.clone();
+                var copia4 = foto.clone();
+
+                size = size/2;
+
+                copia1.crop(0,0,size,size);
+                copia2.crop(size,0,size,size);
+                copia3.crop(0,size,size,size);
+                copia4.crop(size,size,size,size);
+
+                copia1.write("img/"+element+"q.jpg");
+                array.push(element+"q");
+                copia2.write("img/"+element+"r.jpg");
+                array.push(element+"r");
+                copia3.write("img/"+element+"t.jpg");
+                array.push(element+"t");
+                copia4.write("img/"+element+"s.jpg");
+                array.push(element+"s");
+
+                i++;
+                element=array[i];
+                procesarMosaicos (element);
+            }else{
+                procesarAzulejos(array);
+            }
+        })        
+    }
+
+    function procesarAzulejos (array) {
+
+        console.log(array.toString());
+        element = array.shift();
+
+        Jimp.read("img/" + element + ".jpg", function (err, img) {
+            if (err) throw err;
+            size = this.bitmap.width; 
+            if (size>256) {
+                img.resize(256, 256) 
+                    .write("img/" + element  +".jpg"); 
+                procesarAzulejos(array);
+            }
+        });
+    }
 })
-
-
 
 
 http.listen(puerto,function(){
--- modules/patricio.js
+++ modules/patricio.js
@@ -1,8 +1,15 @@
 
 var micodigo = {
 
+    //primero debo leer la imagen a procesar y determinar su tamanio optimo.
 
+    //llevarla al tamanio optimo
+        
+    //cortarla en 4 nivel a nivel
 
+    //cambiar el tamanio de todas a 256*256
+
+    //almacenar todos los datos en base de datos
 
 }
 
